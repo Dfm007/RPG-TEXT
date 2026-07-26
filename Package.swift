fa5704ae57b9a9d2f85f7116f3d5b7b861acc14b
Dfm007 <118625788+Dfm007@users.noreply.github.com>
2026-07-26 08:22:21 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "RPG-TEXT",
+    name: "AppProject",
     platforms: [
-        .iOS(.v15)  // ⭐ 改为 v15（兼容旧版本 Swift）
+        .iOS(.v15)
     ],
     products: [
-        .library(name: "RPG-TEXT", targets: ["RPG-TEXT"])
+        .library(name: "AppProject", targets: ["AppProject"])
     ],
     dependencies: [
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0"))
     ],
     targets: [
         .target(
-            name: "RPG-TEXT",
+            name: "AppProject",
             dependencies: ["ZIPFoundation"],
             path: "App/Sources/AppProject"
         )
